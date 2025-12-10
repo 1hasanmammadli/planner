ee2cc8e134067cca32bc794dc368bb0253d9840c
Hasan Mammadli <1hasanmammadli@gmail.com>
2025-12-10 16:30:59 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,9 @@
-const CACHE_NAME = 'weekly-planner-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('activate', event => {
+  clients.claim();
 });
+
+self.addEventListener('fetch', () => {});
